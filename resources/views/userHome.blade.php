0b28e7e875513807fc0cb1d6894dde98173f3780
Riajul Islam <ritewu2014@gmail.com>
2023-10-03 09:08:21 +0600

    Email Password Reset Done

--- resources/views/userHome.blade.php
+++ resources/views/userHome.blade.php
@@ -6,8 +6,15 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
+
                     <div class="card-body">
-                        You are a User.
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in as User!') }}
                     </div>
                 </div>
             </div>
